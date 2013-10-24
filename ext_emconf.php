1c9554f0c293c94d5639d7565f9cccb9f655b913
thucke <thucke@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-24 16:40:02 +0000

    compatibility now up to TYPO3 6.1.99
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/th_rating/trunk@81036 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.7.7-6.0.99',
+			'typo3' => '4.7.7-6.1.99',
 			'static_info_tables' => '2.1.0',
 		),
 		'conflicts' => array(
