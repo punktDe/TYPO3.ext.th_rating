173dcb81eb8d0e36884f27a94b3c172abb08befa
thucke <thucke@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-29 18:50:06 +0000

    Task #69487: Raise compatibility to TYPO3 7.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/th_rating/trunk@89856 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.7-5.5.99',
-			'typo3' => '6.2.2-7.4.99',
+			'typo3' => '6.2.2-7.5.99',
 			'static_info_tables' => '6.0.7',
 		),
 		'conflicts' => array(
