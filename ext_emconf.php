d964ebbf4ca3ae09c6dd0ee3fb9f8fa2559c0eb2
thucke <thucke@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-09 16:57:37 +0000

    Task #71443: Change PHP compatibility from 5.5 to 5.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/th_rating/trunk@90185 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.7-5.5.99',
+			'php' => '5.5.0-5.6.99',
 			'typo3' => '6.2.2-7.5.99',
 			'static_info_tables' => '6.0.7',
 		),
