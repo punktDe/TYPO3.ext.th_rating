3304f4b398b32c5e08cfcea996b61c3d23f1b1ea
Thomas Hucke <github@thucke.de>
2019-02-14 17:26:22 +0000

    Apply fixes from StyleCI

--- Tests/Domain/Model/StepconfigTest.php
+++ Tests/Domain/Model/StepconfigTest.php
@@ -1,9 +1,10 @@
 <?php
 namespace Thucke\ThRating\Tests\Domain\Model;
+
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2010 Thomas Hucke <thucke@web.de> 
+*  (c) 2010 Thomas Hucke <thucke@web.de>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -24,7 +25,7 @@ namespace Thucke\ThRating\Tests\Domain\Model;
 ***************************************************************/
 
 /**
- * Testcases for Ratingobject 
+ * Testcases for Ratingobject
  *
  * @version 	$Id:$
  * @author		Thomas Hucke <thucke@web.de>
@@ -33,61 +34,64 @@ namespace Thucke\ThRating\Tests\Domain\Model;
  * @scope 		alpha
  * @entity
  */
-class StepconfigTest extends \TYPO3\CMS\Core\Tests\BaseTestCase {
+class StepconfigTest extends \TYPO3\CMS\Core\Tests\BaseTestCase
+{
 
-	/**
-	 * @var string Put the extension name here
-	 */
-	protected $extensionName = 'th_rating';
- 
- 
-    public function setUp() {
+    /**
+     * @var string Put the extension name here
+     */
+    protected $extensionName = 'th_rating';
+
+    public function setUp()
+    {
         //$this->ratingobject = $this->getMock('Thucke\\ThRating\\Domain\\Model\\Ratingobject');
         $this->ratingobject = \TYPO3\CMS\Core\Tests\BaseTestCase::getAccessibleMock('Thucke\\ThRating\\Domain\\Model\\Ratingobject', [], ['tt_news', 'uid']);
         $this->stepconf = $this->objectManager->get('Thucke\\ThRating\\Domain\\Model\\Stepconf');
-		$this->stepconf->setRatingobject($this->ratingobject);
-		$this->stepconf->setSteporder(1);
-		$this->stepconf->setStepweight(2);
-		$this->stepname = \TYPO3\CMS\Core\Tests\BaseTestCase::getAccessibleMock('Thucke\\ThRating\\Domain\\Model\\Stepname', [], [$this->stepconf, 'Step1']);
-		$mockRepository = \TYPO3\CMS\Core\Tests\BaseTestCase::getAccessibleMock('Thucke\\ThRating\\Domain\\Repository\\StepnameRepository');
-		$this->stepconf->injectStepnameRepository($mockRepository);
+        $this->stepconf->setRatingobject($this->ratingobject);
+        $this->stepconf->setSteporder(1);
+        $this->stepconf->setStepweight(2);
+        $this->stepname = \TYPO3\CMS\Core\Tests\BaseTestCase::getAccessibleMock('Thucke\\ThRating\\Domain\\Model\\Stepname', [], [$this->stepconf, 'Step1']);
+        $mockRepository = \TYPO3\CMS\Core\Tests\BaseTestCase::getAccessibleMock('Thucke\\ThRating\\Domain\\Repository\\StepnameRepository');
+        $this->stepconf->injectStepnameRepository($mockRepository);
     }
 
-    public function tearDown() {
+    public function tearDown()
+    {
         unset($this->ratingobject, $this->stepconf);
     }
 
-	/**
-	 * Checks construction of a new rating object
-	 * @test
-	 */
-	public function anInstanceOfTheStepconfigurationHasBeenConstructed() {
-		$this->assertInstanceOf('Thucke\\ThRating\\Domain\\Model\\Stepconf', $this->stepconf);
-		$this->assertSame($this->ratingobject, $this->stepconf->getRatingobject());
-		$this->assertEquals(1, $this->stepconf->getSteporder());
-		$this->assertEquals(2, $this->stepconf->getStepweight());
-		$this->assertEquals('1', strval($this->stepconf->getStepname()));
-	}
+    /**
+     * Checks construction of a new rating object
+     * @test
+     */
+    public function anInstanceOfTheStepconfigurationHasBeenConstructed()
+    {
+        $this->assertInstanceOf('Thucke\\ThRating\\Domain\\Model\\Stepconf', $this->stepconf);
+        $this->assertSame($this->ratingobject, $this->stepconf->getRatingobject());
+        $this->assertEquals(1, $this->stepconf->getSteporder());
+        $this->assertEquals(2, $this->stepconf->getStepweight());
+        $this->assertEquals('1', strval($this->stepconf->getStepname()));
+    }
 
-	/**
-	 * Checks __toString gives steporder
-	 * @test
-	 */
-	public function toStringReturnsSteporder() {
-		$this->assertEquals($this->stepconf, strval($this->stepconf->getStepname()));
-		
-	}
+    /**
+     * Checks __toString gives steporder
+     * @test
+     */
+    public function toStringReturnsSteporder()
+    {
+        $this->assertEquals($this->stepconf, strval($this->stepconf->getStepname()));
+    }
 
-	/**
-	 * Checks __toString gives current stepname
-	 * @test
-	 */
-	public function aStepameCouldBeAdded() {
-		$this->stepconf->addStepname($this->stepname);
-		$this->markTestIncomplete(
-          'This test has not been implemented yet. - '.strval($this->stepconf->getStepname())
+    /**
+     * Checks __toString gives current stepname
+     * @test
+     */
+    public function aStepameCouldBeAdded()
+    {
+        $this->stepconf->addStepname($this->stepname);
+        $this->markTestIncomplete(
+          'This test has not been implemented yet. - ' . strval($this->stepconf->getStepname())
         );
-		$this->assertEquals('Step1', strval($this->stepconf->getStepname()));	
-	}
-	
+        $this->assertEquals('Step1', strval($this->stepconf->getStepname()));
+    }
 }
