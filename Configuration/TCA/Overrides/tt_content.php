9a8b7978b549b69855301e2f15b08af69a585619
thucke <15112795+thucke@users.noreply.github.com>
2019-10-24 18:02:21 +0200

    SonarQube: Fixings

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,15 +1,23 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3_MODE') or die();
 
-call_user_func(function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'Thucke.ThRating',	// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
-            'Pi1',		// A unique name of the plugin in UpperCamelCase
-            'Rating AX'	// A title shown in the backend dropdown field
+call_user_func(static function () {
+    ExtensionUtility::registerPlugin(
+        // The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+        'Thucke.ThRating',
+        'Pi1',		// A unique name of the plugin in UpperCamelCase
+        'Rating AX'	// A title shown in the backend dropdown field
     );
 
     $pluginSignature = 'thrating_pi1';
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,recursive,pages';
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:th_rating/Configuration/Flexforms/flexform_pi1.xml');
+    ExtensionManagementUtility::addPiFlexFormValue(
+        $pluginSignature,
+        'FILE:EXT:th_rating/Configuration/Flexforms/flexform_pi1.xml'
+    );
 });
--- Resources/Public/JavaScript/actions.js
+++ Resources/Public/JavaScript/actions.js
@@ -108,6 +108,7 @@ function checkVoteSubmission(formData, jqForm, options) {
 			return false;
 		}
 		if (formData[i]["name"] == "tx_thrating_pi1[ajaxRef]") {
+      // noinspection JSUnusedLocalSymbols
       let ajaxTargetId = formData[i]["value"];
     }
 	}
@@ -214,10 +215,10 @@ function fadeFlashMessage() {
 function switchStepname( linkObj, hoverIn ) {
 	stepname = jQuery(linkObj).attr('title');
 	if ( hoverIn ) {
-		stepnameObj = jQuery(linkObj).parents('div.tx-thrating-ratinglinks').children('div.stepnameTooltip').first();
+		const stepnameObj = jQuery(linkObj).parents('div.tx-thrating-ratinglinks').children('div.stepnameTooltip').first();
 		jQuery(stepnameObj).html(stepname).addClass('stepnameTooltipOn').removeClass('stepnameTooltip');
 	} else {
-		stepnameObj = jQuery(linkObj).parents('div.tx-thrating-ratinglinks').children('div.stepnameTooltipOn').first();
+		const stepnameObj = jQuery(linkObj).parents('div.tx-thrating-ratinglinks').children('div.stepnameTooltipOn').first();
 		jQuery(stepnameObj).html('&nbsp;').removeClass('stepnameTooltipOn').addClass('stepnameTooltip');
 	}
 }
@@ -229,7 +230,7 @@ function switchStepname( linkObj, hoverIn ) {
 function adjustHeights(currElem) {
 	let fontstep = 1;
 	if (jQuery(currElem).height()>jQuery(currElem).parent().height() || jQuery(currElem).width()>jQuery(currElem).parent().width()) {
-		jQuery('.currentPollText').css('font-size',((jQuery(currElem).css('font-size').substr(0,2)-fontstep)) + 'px');
+		jQuery('.currentPollText').css('font-size',(jQuery(currElem).css('font-size').substr(0,2)-fontstep) + 'px');
 		adjustHeights(jQuery(currElem));
 	}
 }
